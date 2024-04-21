894e7bc35d0a00b350d80baa10b507f970ab0e06
PilotGuy772 <laethenglish@gmail.com>
2024-04-21 11:19:02 -0700

    basic assignments implementation

--- Hackabyte/Assignment.cs
+++ Hackabyte/Assignment.cs
@@ -2,5 +2,14 @@ namespace Hackabyte;
 
 public class Assignment
 {
+    public string Name { get; set; }
+    public DateTime DueDate { get; set; }
+    public TimeSpan TimeEstimate { get; set; }
+
+    public double Priority =>
+        // total amount of time of work needed over total amount of work time until due
+        TimeEstimate / (DueDate - DateTime.Now);
+
+    public static List<Assignment> Assignments = [];
     
 }
---
+++ Hackabyte/AutoScheduler.cs
@@ -0,0 +1,9 @@
+namespace Hackabyte;
+
+public class AutoScheduler
+{
+    /*
+     * This is the basic implementation for the auto-scheduler
+     * 
+     */
+}
